5254179611a4b983881bd7d5c746485f7f3b345a
aigasuryani <71936200+aigasuryani@users.noreply.github.com>
2020-10-03 09:22:49 +0700

    Create if_else.php

---
+++ if_else.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>PENGGUNAAN IF ELSE</title>
+</head>
+<body>
+nilai A = 10 <br>
+nilai B = 5 <br><br>
+<?php
+$a = 10;
+$b = 5;
+if($b < $a)
+{
+echo "B lebih kecil dari A";
+}
+else
+{
+    echo "B lebih besar dari A";
+}
+?>
+</body>
+</html>
